8461cf3e3e3dcbd9a6b10e64c88f09c0c9aa4191
Yu Zhang <atomhust@gmail.com>
2015-02-03 21:03:04 -0800

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Source/Creature.h
@@ -0,0 +1,21 @@
+//
+//  Creature.h
+//  GameOfLife
+//
+//  Created by ZHANG YU on 2/3/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Creature : CCSprite
+
+// stores the current state of the creature
+@property (nonatomic, assign) BOOL isAlive;
+
+// stores the amount of living neighbors
+@property (nonatomic, assign) NSInteger livingNeighbors;
+
+- (id)initCreature;
+
+@end
---
+++ GameOfLife.spritebuilder/Source/Creature.m
@@ -0,0 +1,13 @@
+//
+//  Creature.m
+//  GameOfLife
+//
+//  Created by ZHANG YU on 2/3/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Creature.h"
+
+@implementation Creature
+
+@end
